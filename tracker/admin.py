8cdaffc55129da29b154aed601af62a1df0a9d17
Orutwa <orutwajustine@gmail.com>
2024-06-11 09:59:45 +0300

    table changes

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 from .models import customer, tractor_brand, tractor_model, tractor_details, implements, ownership_history
 
 @admin.register(customer)
